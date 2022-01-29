c5f55c2df2f774851396a6b6e7cdf9f19d904cbe
Julieta Ramos <43016164+Jlsraa@users.noreply.github.com>
2022-01-28 18:08:07 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// Author: Julieta Ramos Ayón
+// Date: 1/28/2022
 
 import 'dart:math';
 import 'dart:ui';
